685994e70d203ed1c051f5622a5e0bdd8100862a
zeck_182 <candra_nova_182@yahoo.co.id>
2023-02-18 01:43:29 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/techciosvenue.github.io/",
+  // base: "/techciosvenue.github.io/",
   plugins: [vue()],
   resolve: {
     alias: {
